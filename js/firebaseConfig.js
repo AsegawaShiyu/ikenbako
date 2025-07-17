9a6b8f868838d847965f3387e05e76283d18429a
AsegawaShiyu <84178195+AsegawaShiyu@users.noreply.github.com>
2025-07-17 14:47:51 +0900

    Create firebaseConfig.js

---
+++ js/firebaseConfig.js
@@ -0,0 +1,16 @@
+// Firebase SDKを初期化するファイル
+
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.10.0/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/11.10.0/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAkwrV-roOMxuBhVjwLoBYq17YPl5kaDJg",
+  authDomain: "ikenbako-28697.firebaseapp.com",
+  projectId: "ikenbako-28697",
+  storageBucket: "ikenbako-28697.appspot.com",
+  messagingSenderId: "336819870479",
+  appId: "1:336819870479:web:7ecbca02ff3e3b5534bc7a"
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
