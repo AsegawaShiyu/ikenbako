d234835e18d9e445181f9e0c51a5c55f6cf0cf6f
AsegawaShiyu <84178195+AsegawaShiyu@users.noreply.github.com>
2025-07-17 15:52:29 +0900

    Update firebaseConfig.js

--- js/firebaseConfig.js
+++ js/firebaseConfig.js
@@ -2,12 +2,12 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/11.10.0/fireba
 import { getFirestore } from "https://www.gstatic.com/firebasejs/11.10.0/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "...",
-  authDomain: "...",
-  projectId: "...",
-  storageBucket: "...",
-  messagingSenderId: "...",
-  appId: "..."
+  apiKey: "AIzaSyAkwrV-roOMxuBhVjwLoBYq17YPl5kaDJg",
+  authDomain: "ikenbako-28697.firebaseapp.com",
+  projectId: "ikenbako-28697",
+  storageBucket: "ikenbako-28697.firebasestorage.app",
+  messagingSenderId: "336819870479",
+  appId: "1:336819870479:web:7ecbca02ff3e3b5534bc7a"
 };
 
 const app = initializeApp(firebaseConfig);
